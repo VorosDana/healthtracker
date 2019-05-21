afa6890d12d56f4a8fc98a843f4a8241d30aff1c
Dana Voros <vorosdana@gmail.com>
2019-05-21 12:58:46 -0700

    Initial buildout completed, tapping works and so does the image carousel

--- app/src/main/java/com/example/healthtracker/MainActivity.java
+++ app/src/main/java/com/example/healthtracker/MainActivity.java
@@ -3,12 +3,100 @@ package com.example.healthtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int taps;
+    private Carousel images;
+    private Button tapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tapButton = findViewById(R.id.button_tap);
+        taps = 0;
+
+        images = new Carousel();
+        images.add(R.drawable.royal_cat, "Tap for your queen!");
+        images.add(R.drawable.shadowy_cat, "The cat watches. The cat approves of your tapping.");
+        images.add(R.drawable.sleepy_cat, "The cat sleeps now. You sleep after you tap!");
+    }
+
+
+
+    public void leftButtonHandler(View in) {
+        images.prev();
+    }
+
+    public void rightButtonHandler(View in) {
+        images.next();
+    }
+
+    public void tapHandler(View in) {
+        taps++;
+        tapButton.setText(taps + " taps! Keep going!");
+    }
+
+    private class Carousel{
+        private Node current;
+        private Node head;
+        private Node tail;
+        ImageView display = findViewById(R.id.image_carousel);
+        TextView captionHolder = findViewById(R.id.caption_holder);
+
+        public void add(int imageId, String caption) {
+            if(tail == null){
+                tail = new Node();
+                tail.imageId = imageId;
+                tail.index = 1;
+                tail.caption = caption;
+                head = tail;
+                tail.next = tail;
+                tail.prev = tail;
+                current = tail;
+                show();
+                return;
+            }
+
+            Node node = new Node();
+            node.caption = caption;
+            node.prev = tail;
+            tail.next = node;
+            node.imageId = imageId;
+            node.index = tail.index + 1;
+            tail = node;
+            tail.next = head;
+            head.prev = tail;
+            show();
+        }
+
+        public void next() {
+            current = current.next;
+            show();
+        }
+
+        public void prev() {
+            current = current.prev;
+            show();
+        }
+
+        private void show(){
+            display.setImageResource(current.imageId);
+            captionHolder.setText("Image " + current.index + "/" +tail.index + "\n" +
+                                        current.caption);
+        }
+    }
+
+    private class Node {
+        Node next;
+        Node prev;
+        int index;
+        int imageId;
+        String caption;
     }
 }
